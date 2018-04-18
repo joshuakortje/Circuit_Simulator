3426fd4179d5e47b65691e986cecb84714c7d1c1
joshuakortje <38233249+joshuakortje@users.noreply.github.com>
2018-04-17 23:10:21 -0400

    WORKING CODE!!! It runs files 0, 1, 2 ,7, and 8 correctly. I didn't check the others yet.

--- Wire.cpp
+++ Wire.cpp
@@ -3,7 +3,7 @@
 #include "Gate.h"
 
 // Constructor
-Wire::Wire(int newNumber, string name) : number(newNumber), name(name) {
+Wire::Wire(int newNumber, string name, bool InOut) : number(newNumber), name(name), isInOut(InOut) {
 	// intitialize the value and number
 	//there are no known gates so we leave the vector empty
 	value = UNKNOWN;
@@ -13,6 +13,12 @@ Wire::Wire(int newNumber, string name) : number(newNumber), name(name) {
 int Wire::getValue(){
 	// return the value on the Wire
 	return value;
+}
+
+// checks if the wire is an input or output
+bool Wire::getInOut() {
+	// returns true if it is an input or output wire
+	return isInOut;
 }
 
 // Changes the Wire value
@@ -27,6 +33,24 @@ void Wire::addGate(Gate * newGate){
 	nextGates.push_back(newGate);
 }
 
+// returns all the gates driven by the wire
 vector<Gate *> Wire::getNextGates() {
 	return nextGates;
+}
+
+//add history to the wire
+void Wire::addHistory(int newHistory) {
+	history.push_back(newHistory);
+}
+
+//return the front of the vector and remove it
+int Wire::readHistory() {
+	if (history.size() == 0) {
+		return EMPTY_HISTORY;
+	}
+	else {
+		int temp = history.front();
+		history.erase(history.begin());
+		return temp;
+	}
 }
--- Wire.h
+++ Wire.h
@@ -15,18 +15,24 @@ const int EMPTY_HISTORY = -2;
 
 class Wire {
 public:
-	Wire(int newNumber, string name);
+	Wire(int newNumber, string name, bool InOut);
 	int getValue();
+	bool getInOut();
 	int getNumber() const { return number; }
 	string getName() const { return name; }
 	void updateWire( int newValue);
 	void addGate(Gate* newGate);
 	vector<Gate *> getNextGates();
+	void addHistory(int newHistory);
+	int readHistory();
+
 private:
+	bool isInOut; //true for inputs and outputs
 	int value;
 	int number;
 	string name;
 	vector<Gate *> nextGates;
+	vector<int> history;
 };
 
 #endif
